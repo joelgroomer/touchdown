5a15f83745a1957bddb68605f8b59b507f9d3471
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-02 00:22:26 -0800

    Add color constants

---
+++ Touchdown/Touchdown/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Joel Groomer on 1/2/24.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
