f0e8533b92a8516080ceaacb386cb23355735cdf
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-02 00:00:00 -0800

    Create project

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Joel Groomer on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
