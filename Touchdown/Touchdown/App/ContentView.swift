c23f5580f7d7067d9c8cf3b5cc77f1f42f84455e
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-07 22:58:32 -0800

    Add BrandGridView to ContentView

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -38,6 +38,10 @@ struct ContentView: View {
                         } //: LazyVGrid
                         .padding(15)
                         
+                        TitleView(title: "Brands")
+                        
+                        BrandGridView()
+                        
                         FooterView()
                             .padding(.horizontal)
                     } //: VStack
