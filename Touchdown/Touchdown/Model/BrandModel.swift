061788b949f3d287858d1674969e992b8edf7195
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-07 22:41:42 -0800

    Add Brand model

---
+++ Touchdown/Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Joel Groomer on 1/7/24.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
